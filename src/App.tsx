0bb6eb823d282593f33315e420414d105a438369
wanying.wang <wanying.wang@17zuoye.com>
2023-07-15 11:27:02 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div>
+      <h3>App</h3>
+      <i className="icon iconfont icon-wenben"></i>
+      <div className="test">12222222</div>
+    </div>
+  );
+}
